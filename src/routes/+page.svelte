4afc8f3ef607b46f7da324b60c835321499a5f9c
Tarik <tarikmerrylees@gmail.com>
2024-03-12 14:20:29 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Screen Fu Masters</h1>
+	</div>
+</div>
+
+<nav>
+
+	<!-- Navigation links to apps: Basic Standard Combats trainer, Standard Combats trainer with Transitions   -->
+	<ul class="flex space-x-5">
+		<li><a href="/standard">Standard Combats</a></li>
+		<li><a href="/standard-transitions">SCs with Transitions</a></li>
+		<li><a href="/unarmed">Unarmed Combat</a>
+</nav>
