76db7cc777d6d6f9a841b46e07d763bf894f5bab
Tarik <tarikmerrylees@gmail.com>
2024-03-12 14:46:42 +0000

    style navigation section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,27 @@
+<script>
+	const combat_trainers = [
+		{name: 'Standard Combats', route: '/standard'},
+		{name: 'SCs with Transitions', route: '/standard-transitions'},
+		{name: 'Unarmed Combat', route: '/unarmed'}
+	]
+</script>
+
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1">Screen Fu Masters</h1>
 	</div>
 </div>
 
-<nav>
 
-	<!-- Navigation links to apps: Basic Standard Combats trainer, Standard Combats trainer with Transitions   -->
-	<ul class="flex space-x-5">
-		<li><a href="/standard">Standard Combats</a></li>
-		<li><a href="/standard-transitions">SCs with Transitions</a></li>
-		<li><a href="/unarmed">Unarmed Combat</a>
-</nav>
+<!-- css tailwaind container for a nav section and its title -->
+<div class="container flex flex-col justify-center items-center w-full mx-auto py-3 md:max-w-sm border-solid border-white border-2 rounded-lg">
+	<h2 class="text-xl pb-1 font-bold">Combat Trainers</h2>
+	<nav class="w-5/6 mx-auto flex flex-col justify-center items-center bg-primary-900 p-4 rounded-lg">
+		<ul class="flex-col mx-auto text-center">
+			<!-- loop through combat_trainers data to create anchors-->
+			{#each combat_trainers as trainer}
+			<li class="hover:text-warning-700"><a href={trainer.route}>{trainer.name}</a></li>
+			{/each}
+			</ul>
+	</nav>
+</div>
