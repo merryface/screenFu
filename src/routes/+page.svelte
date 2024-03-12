ecac3c86d684b3fc8b57b78cc6024cc725c2e25f
Tarik <tarikmerrylees@gmail.com>
2024-03-12 14:49:27 +0000

    style navigation section and homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,22 +6,21 @@
 	]
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Screen Fu Masters</h1>
-	</div>
-</div>
-
-
-<!-- css tailwaind container for a nav section and its title -->
-<div class="container flex flex-col justify-center items-center w-full mx-auto py-3 md:max-w-sm border-solid border-white border-2 rounded-lg">
-	<h2 class="h2">Combat Trainers</h2>
-	<nav class="w-5/6 mx-auto flex flex-col justify-center items-center bg-primary-900 p-4 rounded-lg">
-		<ul class="flex-col mx-auto text-center">
-			<!-- loop through combat_trainers data to create anchors-->
-			{#each combat_trainers as trainer}
-			<li class="hover:text-warning-700"><a href={trainer.route}>{trainer.name}</a></li>
-			{/each}
+<div class="container pt-5 h-full mx-auto">
+	<h1 class="h1 text-center mb-5">Screen Fu Masters</h1>
+	
+	
+	<!-- css tailwaind container for a nav section and its title -->
+	<div class="container flex flex-col justify-center items-center w-full mx-auto py-3 md:max-w-sm border-solid border-white border-2 rounded-lg">
+		<h2 class="h2 mb-3">Combat Trainers</h2>
+		<nav class="w-5/6 mx-auto flex flex-col justify-center items-center bg-primary-900 p-4 rounded-lg">
+			<ul class="flex-col mx-auto text-center">
+				<!-- loop through combat_trainers data to create anchors-->
+				{#each combat_trainers as trainer}
+				<li class="hover:text-warning-700"><a href={trainer.route}>{trainer.name}</a></li>
+				{/each}
 			</ul>
-	</nav>
+		</nav>
+	</div>
+	
 </div>
