c171ad82868bd7649366692b4fd94a25e42321a0
Tarik <tarikmerrylees@gmail.com>
2024-03-12 15:44:39 +0000

    minor 404 styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,2 +1,10 @@
 
 <p>The page was not found. Go back to the <a href="/" class="text-blue-500 underline hover:text-blue-700">Home page</a></p>
+
+<style>
+  p {
+    margin: auto;
+    margin-top: 3rem;
+    text-align: center;
+  }
+</style>
