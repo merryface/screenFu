0761c2c21298631b0ec790111eb44e49ad482774
Tarik <tarikmerrylees@gmail.com>
2024-03-14 17:04:54 +0000

    typo  correction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 		<h2 class="h2 mb-2">Combat Trainers</h2>
 		<p>The Screen Fu Masters trainer. Practice the things you've learned with the Stage Fight folks in preparation for your next course.</p>
 		<p>Use the navlinks on the left to get you to the area you need to be.</p>
-		<p>Ready for the next course? Book in with Stage fight by clicking on the link bottom left.</p>
+		<p>Ready for the next course? Book in with Stage fight by clicking on the link in the bottom left.</p>
 
 	</div>
 </div>
